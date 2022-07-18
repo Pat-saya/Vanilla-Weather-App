aa85074cc640280c2422ac9facc76de2c66d0620
Pat-saya <Patsaya.w@gmail.com>
2022-07-17 19:47:39 -0700

    showing the current temperature.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-alert("hello");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#windSpeed");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "72fd30af3b4ddff7c47814422771b83e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
